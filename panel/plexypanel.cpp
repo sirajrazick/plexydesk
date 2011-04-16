61fc806b29af2568dbfa481d9349ba15cda3f3b5
Varuna Lekamwasam <vrlekamwasam@gmail.com>
2011-04-17 01:21:01 +0530

    Fixed the branch error with copyright message

--- panel/plexypanel.cpp
+++ panel/plexypanel.cpp
@@ -1,13 +1,30 @@
+/*******************************************************************************
+* This file is part of PlexyDesk.
+*  Maintained by : Siraj Razick <siraj@kde.org>
+*  Authored By  : Varuna Lekamwasam <vrlekamwasam@gmail.com>
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with PlexyDesk. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
+
+
 #include "plexypanel.h"
 
 #include <pluginloader.h>
+
 #include <QDesktopWidget>
 #include <QDebug>
-
-//includes needed for testing
 #include <QLabel>
-//#include <QTextEdit>
-//#include <QPushButton>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QVariant>
